3acf446f69bf1402706d5224ea5414400097820a
Baxter <s4171523@student.rmit.edu.au>
2026-02-23 12:27:40 +1100

    map is all walls with clearing for teleporter, middle box and surrounding area

--- Game.py
+++ Game.py
@@ -12,7 +12,7 @@ def StartGame(frmLobby, window, height, width):
     for i in range(height):
         for j in range(width):
             if field[i][j] == -1:
-                lblField[i][j].config(bg="black")
+                lblField[i][j].config(bg="blue", text=f"({i},{j})")
             lblField[i][j].grid(row=i, column=j)
             print(f'{field[i][j]:4}' , end=", ")
         print()
@@ -21,40 +21,40 @@ def StartGame(frmLobby, window, height, width):
 
 
 def GenerateField(height, width):
-    field = [[0 for _ in range(height)] for _ in range(width)]
+    field = [[-1 for _ in range(height)] for _ in range(width)]
     # outlide walls need to be set
     # custom maps
     # seed random to generate
     # wall = -1
     # gate = -2
 
-    # place outside walls
-    for i in range(width):
-        field[0][i] = -1
-        field[i][0] = -1
-        field[height - 1][i] = -1
-        field[i][height - 1] = -1
-
     # open teleport
-    middleHeight = height // 2 + 1
+    middleHeight = height // 2
     field[middleHeight][0] = 0
     field[middleHeight][width-1] = 0
 
     # middle box
-    middleWidth = width // 2 + 1
-    field[middleWidth][0] = 0
-    field[middleWidth][height-1] = 0
+    middleWidth = width // 2
+    print(middleWidth)
+    field[middleHeight][middleWidth] = 0
+    field[middleHeight][middleWidth - 1] = 0
+    field[middleHeight][middleWidth + 1] = 0
 
-    for i in range(3):
-        field[middleHeight - 1][middleWidth - i] = -1
-        field[middleHeight - 1][middleWidth + i] = -1
-        field[middleHeight + 1][middleWidth - i] = -1
-        field[middleHeight + 1][middleWidth + i] = -1
+    # clear around box
+    for i in range(-3,4): # not inclusive for 4
+        field[middleHeight - 2][middleWidth + i] = 0
+        field[middleHeight + 2][middleWidth + i] = 0
 
-    field[middleHeight][middleWidth + 2] = -1
-    field[middleHeight][middleWidth - 2] = -1
+    for i in range(-1, 2):  # not inclusive for 2
+        pass
+        field[middleHeight + i][middleWidth - 3] = 0
+        field[middleHeight + i][middleWidth + 3] = 0
 
-        # box gate
+    # box gate
     field[middleHeight - 1][middleWidth] = -2
+
+    # seed random
+
+    # select 2-14 branch spots
 
     return field
