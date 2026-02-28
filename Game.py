31f2c52f592ba14487771a1920943eedb5b69f40
Baxter <s4171523@student.rmit.edu.au>
2026-03-01 07:20:15 +1100

    link list working and looping through list till empty

--- Game.py
+++ Game.py
@@ -13,7 +13,7 @@ def StartGame(frmLobby, window, height, width, seed):
 
     for i in range(height):
         for j in range(width):
-            match field[i][j]:
+            match field[i][j][0]:
                 case 1:
                     lblField[i][j].config(bg="white", text=f"({i},{j})")
                 case 0:
@@ -22,16 +22,19 @@ def StartGame(frmLobby, window, height, width, seed):
                     lblField[i][j].config(bg="blue", text=f"({i},{j})")
                 case -4:
                     lblField[i][j].config(bg="red", text=f"({i},{j})")
+                case -5:
+                    lblField[i][j].config(bg="orange", text=f"({i},{j})")
 
             lblField[i][j].grid(row=i, column=j)
-            print(f'{field[i][j]:4}' , end=", ")
+            print(f'{field[i][j][0]:4}' , end=", ")
         print()
 
     frmGame.pack()
 
 
 def GenerateField(height, width, seed):
-    field = [[-1 for _ in range(height)] for _ in range(width)]
+    # nil = not in list for revealed
+    field = [[[-1,"nil"] for _ in range(height)] for _ in range(width)]
     # outlide walls need to be set
     # custom maps
     # seed random to generate
@@ -41,33 +44,33 @@ def GenerateField(height, width, seed):
 
     # open teleport
     middleHeight = height // 2
-    field[middleHeight][0] = 0
-    field[middleHeight][width-1] = 0
+    field[middleHeight][0][0] = 0
+    field[middleHeight][width-1][0] = 0
 
     # middle box
     middleWidth = width // 2
     print(middleWidth)
-    field[middleHeight][middleWidth] = 0
-    field[middleHeight][middleWidth - 1] = 0
-    field[middleHeight][middleWidth + 1] = 0
+    field[middleHeight][middleWidth][0] = 0
+    field[middleHeight][middleWidth - 1][0] = 0
+    field[middleHeight][middleWidth + 1][0] = 0
 
     branchPossible = []
 
     # clear around box
     for i in range(-3,4): # not inclusive for 4
-        field[middleHeight - 2][middleWidth + i] = 0
+        field[middleHeight - 2][middleWidth + i][0] = 0
         # add block above to list for branch
         branchPossible.append([middleHeight - 3, middleWidth + i, "up"])
-        field[middleHeight + 2][middleWidth + i] = 0
+        field[middleHeight + 2][middleWidth + i][0] = 0
         # add block below to list for branch
         branchPossible.append([middleHeight + 3, middleWidth + i, "down"])
 
     for i in range(-2, 3):  # not inclusive for 2
         pass
-        field[middleHeight + i][middleWidth - 3] = 0
+        field[middleHeight + i][middleWidth - 3][0] = 0
         # add block to the left to list for branch
         branchPossible.append([middleHeight + i, middleWidth - 4, "left"])
-        field[middleHeight + i][middleWidth + 3] = 0
+        field[middleHeight + i][middleWidth + 3][0] = 0
         # add block to the right to list for branch
         branchPossible.append([middleHeight + i, middleWidth + 4, "right"])
 
@@ -75,10 +78,10 @@ def GenerateField(height, width, seed):
     # temp for testing set block red if can be branch
     print(branchPossible)
     for branch in branchPossible:
-        field[branch[0]][branch[1]] = -4
+        field[branch[0]][branch[1]][0] = -4
 
     # box gate
-    field[middleHeight - 1][middleWidth] = -2
+    field[middleHeight - 1][middleWidth][0] = -2
 
     # seed random
     random.seed(seed)
@@ -99,14 +102,14 @@ def GenerateField(height, width, seed):
 
         # check if valid
         valid = True
-        if field[attemptBranch[0] - 1][attemptBranch[1]] == 1 or field[attemptBranch[0] + 1][attemptBranch[1]] == 1:
+        if field[attemptBranch[0] - 1][attemptBranch[1]][0] == 1 or field[attemptBranch[0] + 1][attemptBranch[1]][0] == 1:
             valid = False
-        if field[attemptBranch[0]][attemptBranch[1] - 1] == 1 or field[attemptBranch[0]][attemptBranch[1] + 1] == 1:
+        if field[attemptBranch[0]][attemptBranch[1] - 1][0] == 1 or field[attemptBranch[0]][attemptBranch[1] + 1][0] == 1:
             valid = False
 
         # if valid place branch
         if valid:
-            field[attemptBranch[0]][attemptBranch[1]] = 1
+            field[attemptBranch[0]][attemptBranch[1]][0] = 1
 
             # extend branch one block out
             j = 0
@@ -120,18 +123,40 @@ def GenerateField(height, width, seed):
                     k = -1
                 case "right":
                     k = 1
-            field[attemptBranch[0] + j][attemptBranch[1] + k] = 1
+            attemptBranch[0] += j
+            attemptBranch[1] += k
+            field[attemptBranch[0]][attemptBranch[1]][0] = 1
             if i == 0:
-                llBranchesHead = branch(attemptBranch)
+                llBranchesHead = Branch(attemptBranch)
                 llBranchesTail = llBranchesHead
             else:
-                llBranchesTemp = branch(attemptBranch)
-                llBranchesTail.next(llBranchesTemp)
+                llBranchesTemp = Branch(attemptBranch)
+                llBranchesTail.next = llBranchesTemp
                 llBranchesTail = llBranchesTemp
 
 
 
     # for linked list fill area
+    BranchHead(llBranchesHead, field, random)
 
 
     return field
+
+def BranchHead(llBranchesHead, field, random):
+    # loop till list is empty
+
+    while llBranchesHead != None:
+        location = llBranchesHead.data
+        direction = [[-1,0],[1,0],[0,1],[0,-1]]
+
+        # loop for all directions
+        while direction != []:
+            directionID = random.randint(0, len(direction) - 1)
+            checkDirection = direction[directionID]
+            blockCheck = [location[0] + checkDirection[0], location[1] + checkDirection[1]]
+            field[blockCheck[0]][blockCheck[1]][0] = -5
+            direction.pop(directionID)
+
+        # move to next node
+        llBranchesHead = llBranchesHead.next
+        print("loop")
--- LinkedList.py
+++ LinkedList.py
@@ -1,4 +1,7 @@
-class branch:
+class Branch:
     def __init__(self, data):
         self.data = data
         self.next = None # Pointer to the next node, initialized to None
+
+    def next(self, next):
+        self.next = next
